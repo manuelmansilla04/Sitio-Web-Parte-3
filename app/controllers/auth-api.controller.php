1ba3e730bf7860b2c37708a01dde3c53ed54d9be
manuelmansilla04 <manuelmansilla2004@gmail.com>
2026-06-28 17:09:12 -0300

    Refactor

--- app/controllers/auth-api.controller.php
+++ app/controllers/auth-api.controller.php
@@ -11,13 +11,11 @@ class AuthApiController {
     }
 
     public function login($request, $response) {
-        // Leer el ecabezado Authorization
         $authorization = $request->authorization;
 
-        // Chequear que el encabezado sea 'Basic base64(user:pass)'
+        //Chequeo de que el encabezado sea 'Basic base64(user:pass)'
         $auth = explode(' ', $authorization);
         if (count($auth) != 2 || $auth[0] !== 'Basic') {
-            // Si no es correcto, devolver error 401 Unauthorized
             header("WWW-Authenticate: Basic realm='Get a token'");
             return $response->json("Autenticación no valida", 401);
         }
@@ -25,26 +23,24 @@ class AuthApiController {
         $auth = base64_decode($auth[1]); // "user:pass"
         $user_pass = explode(":", $auth);
         if (count($user_pass) != 2) {
-            // Si no es correcto, devolver error 401 Unauthorized
             return $response->json("Autenticación no valida", 401);
         }
 
         $user = $user_pass[0];
         $password = $user_pass[1];
-        // Buscar el usuario en la DB
         $userFromDB = $this->userModel->getByUser($user);
         
-        // verificar la contreaseña con la de la base (hasheada)
-        if(!$userFromDB || !password_verify($password, $userFromDB->password)) {
+        //verificar la contreaseña con la de la base hasheada
+        if (!$userFromDB || !password_verify($password, $userFromDB->password)) {
             return $response->json("Usuario o contraseña incorrecta", 401);
         }
 
-        // Si todo está bien, devolvemos un token
+        //Todo el orden? Tomá un token
         $payload = [
             'sub' => $userFromDB->ID,
             'usuario' => $userFromDB->user_name,
             'roles' => ['ADMIN', 'USER', 'MESSI'],
-            'exp' => time() + 3600 // Expira en 1 hora
+            'exp' => time() + 3600 //1hs para expirar
         ];
 
         return $response->json(createJWT($payload));
